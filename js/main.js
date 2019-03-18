42cb4e0f254f0a645f29a557ce1a1c4fd456fd8c
AndrewKamajaya <41249048+AndrewKamajaya@users.noreply.github.com>
2019-03-17 23:17:49 -0400

    Document

---
+++ js/main.js
@@ -0,0 +1,95 @@
+(function($) {
+  
+  "use strict";
+
+/* 
+   CounterUp
+   ========================================================================== */
+    $('.counter').counterUp({
+      time: 3500
+    });  
+
+/* 
+   Sticky Nav
+   ========================================================================== */
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.header-top-area').addClass('menu-bg');
+        } else {
+            $('.header-top-area').removeClass('menu-bg');
+        }
+    });
+
+/* 
+   Back Top Link
+   ========================================================================== */
+    var offset = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1000);
+      return false;
+    })
+
+/* 
+   One Page Navigation & wow js
+   ========================================================================== */
+    //Initiat WOW JS
+    new WOW().init();
+
+    // one page navigation 
+    $('.main-navigation').onePageNav({
+            currentClass: 'active'
+    }); 
+
+    $(window).on('load', function() {
+       
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 200) {
+                $('.fixed-top').addClass('menu-bg');
+            } else {
+                $('.fixed-top').removeClass('menu-bg');
+            }
+        });
+
+    });
+/* Nivo Lightbox
+  ========================================================*/   
+   $('.lightbox').nivoLightbox({
+    effect: 'fadeScale',
+    keyboardNav: true,
+  });
+
+
+/* stellar js
+  ========================================================*/
+  $.stellar({
+    horizontalScrolling: false,
+    verticalOffset: 530,
+    responsive: false
+  });
+
+/* 
+   Page Loader
+   ========================================================================== */
+   $(window).on('load',function() {
+      "use strict";
+      $('#loader').fadeOut();
+    });
+
+}(jQuery));
+
